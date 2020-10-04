61a795add28b2491e9748de4c10d3a5e40400b6e
nabiharaza <nabiha_137@hotmail.com>
2020-10-04 12:39:23 -0400

    Mapped Left Matrix - Didn't test all the code fully. Commented out the failed_nodes and vectors

--- XCode.py
+++ XCode.py
@@ -1,8 +1,9 @@
-def input(N):
-    left_matrix = [["(" + str(j) + "," + str(i) + ")" for i in range(N)] for j in range(N)]
+def input(matrix_dimensions):
+    left_matrix = [["(" + str(j) + "," + str(i) + ")" for i in range(matrix_dimensions)] for j in
+                   range(matrix_dimensions)]
     right_matrix = left_matrix
-    print("Input:\n", left_matrix)
-    print("--------------------------------------------------------------------------------------------------------\n")
+    # print("Input:\n", left_matrix)
+    # print("--------------------------------------------------------------------------------------------------------\n")
     return left_matrix, right_matrix
 
 
@@ -17,7 +18,7 @@ def mapping(left_matrix, right_matrix):
 
 
 def left_mapping(left_matrix):
-    print("Left Mapping")
+    # print("Left Mapping")
     col = 0
     mapping_list = []
     n = len(left_matrix)
@@ -30,9 +31,9 @@ def left_mapping(left_matrix):
         # print("first pass seq: ", row_sequence)
         row_sequence = left_matrix_second_pass(row_sequence, right_part_row, right_part_col, n)
         # print("second pass seq: ", row_sequence)
-        print("Left matrix mapping seq: ", row_sequence)
-        mapping_list.append(row_sequence)
-    print("--------------------------------------------------------------------------------------------------------\n")
+    #     print("Left matrix mapping seq: ", row_sequence)
+    #     mapping_list.append(row_sequence)
+    # print("--------------------------------------------------------------------------------------------------------\n")
     return mapping_list
 
 
@@ -80,7 +81,7 @@ def right_matrix_second_pass(row_sequence, row, col, n):
 
 
 def right_mapping(right_matrix):
-    print("Right Mapping")
+    # print("Right Mapping")
     col = 0
     mapping_list = []
     n = len(right_matrix)
@@ -93,9 +94,9 @@ def right_mapping(right_matrix):
         # print("first pass seq: ", row_sequence)
         row_sequence = right_matrix_second_pass(row_sequence, right_part_row, right_part_col, n)
         # print("second pass seq: ", row_sequence)
-        print("Right matrix mapping seq: ", row_sequence)
-        mapping_list.append(row_sequence)
-    print("--------------------------------------------------------------------------------------------------------\n")
+    #     print("Right matrix mapping seq: ", row_sequence)
+    #     mapping_list.append(row_sequence)
+    # print("--------------------------------------------------------------------------------------------------------\n")
     return mapping_list
 
 
@@ -107,13 +108,20 @@ def rotate_left_matrix(left_matrix):
     print("Rotate Left Matrix")
     whole_column_matrix = []
 
+    tracker = []
     for col in range(len(left_matrix)):
         # create column vectors
-        col_sequence = [item[col] for item in left_matrix]
+        col_sequence = [row[col] for row in left_matrix]
+        # implementing mapping/index_tracker
+        index_tracker = [row for row in range(len(left_matrix))]
 
         # Rotate every column vector by i = (n-1) - c
         i = len(left_matrix) - 1 - col
         col_sequence = col_sequence[i:] + col_sequence[:i]
+        index_tracker = index_tracker[i:] + index_tracker[:i]
+        # print("row rotated", index_tracker)
+        tracker.append(index_tracker)
+
         whole_column_matrix.append(col_sequence)
 
     final_left_rotated_matrix = []
@@ -122,9 +130,18 @@ def rotate_left_matrix(left_matrix):
     for row in range(len(whole_column_matrix)):
         row_sequence = [item[row] for item in whole_column_matrix]
         final_left_rotated_matrix.append(row_sequence)
-        print(row_sequence)
-    print("--------------------------------------------------------------------------------------------------------\n")
-    return final_left_rotated_matrix
+    #     print(row_sequence)
+    # print("--------------------------------------------------------------------------------------------------------\n")
+    map_left_rotated_to_original, itr = [], 0
+    for row in range(len(tracker)):
+        row_sequence = []
+        itr = 0
+        for item in range(len(tracker)):
+            row_sequence.append([tracker[item][row], itr])
+            itr += 1
+        map_left_rotated_to_original.append(row_sequence)
+        print(map_left_rotated_to_original[row])
+    return final_left_rotated_matrix, map_left_rotated_to_original
 
 
 def rotate_right_matrix(right_matrix):
@@ -149,8 +166,8 @@ def rotate_right_matrix(right_matrix):
     for row in range(len(whole_column_matrix)):
         row_sequence = [item[row] for item in whole_column_matrix]
         final_right_rotated_matrix.append(row_sequence)
-        print(row_sequence)
-    print("--------------------------------------------------------------------------------------------------------\n")
+    #     print(row_sequence)
+    # print("--------------------------------------------------------------------------------------------------------\n")
     return final_right_rotated_matrix
 
 
@@ -180,10 +197,14 @@ def get_failed_nodes(failed_vectors, left_rotated_matrix, right_rotated_matrix):
     for col in failed_vectors:
         for row in range(len(right_rotated_matrix)):
             # leaving out the secondary diagonals to be added in failed nodes
-            if ((row + col) != (len(right_rotated_matrix) - 1)):
+            if (row + col) != (len(right_rotated_matrix) - 1):
                 right_failed_nodes.add(tuple((row, col)))
-    print("Left failed Nodes: ", left_failed_nodes, "and right failed nodes: ", right_failed_nodes)
+    # print("Left failed Nodes: ", left_failed_nodes, "and right failed nodes: ", right_failed_nodes)
     return left_failed_nodes
+
+
+def matrix_mapper_original_and_rotated(left_matrix, right_matrix, left_rotated_matrix, right_rotated_matrix):
+    pass
 
 
 if __name__ == '__main__':
@@ -191,6 +212,7 @@ if __name__ == '__main__':
     mapped_left_matrix = mapping(left_matrix, right_matrix)
     left_rotated_matrix, right_rotated_matrix = rotate(left_matrix, right_matrix)
 
-    failed_vectors = [1, 3]
-    failed_nodes = get_failed_nodes(failed_vectors, left_rotated_matrix, right_rotated_matrix)
-    recovery_process(failed_vectors, failed_nodes, left_rotated_matrix, right_rotated_matrix)
+    # failed_vectors = [1, 3]
+    # failed_nodes = get_failed_nodes(failed_vectors, left_rotated_matrix, right_rotated_matrix)
+    # recovery_process(failed_vectors, failed_nodes, left_rotated_matrix, right_rotated_matrix)
+    # matrix_mapper_original_and_rotated(left_matrix, right_matrix, left_rotated_matrix, right_rotated_matrix)
